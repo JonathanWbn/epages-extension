9fb6957eef85f0e1e0c04d4b653cd468aa45f5ff
Jonathan Wieben <joni.wieben@icloud.com>
2019-11-15 20:11:18 +0100

    Chore: Improve background epages detection

--- extension/background-content.js
+++ extension/background-content.js
@@ -5,9 +5,13 @@ window.browser = window.browser || window.chrome;
 browser.runtime.onMessage.addListener((msg, _sender, sendResponse) => {
   if (msg.text === "is_epages_shop") {
     const head = document.getElementsByTagName("head")[0];
-    const occurencesOfEpages = head.innerHTML.match(/epages/g).length;
 
-    if (occurencesOfEpages > 5) sendResponse(true);
+    const looksLikeNowOrBeyondShop =
+      head.innerHTML.match(/epages\.base/g) &&
+      head.innerHTML.match(/epages\.base/g).length > 4;
+    const looksLikeBaseShop = head.innerHTML.includes("epages.js");
+
+    if (looksLikeNowOrBeyondShop || looksLikeBaseShop) sendResponse(true);
     else sendResponse(false);
   }
 });
